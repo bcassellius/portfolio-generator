d2c6c6a901499bb30da0a2e87b914841593dc03c
Bri <bhilliker@gmail.com>
2021-07-21 14:51:58 -0500

    create html template and generate page, modularize code

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template')
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
+const [name, github] = profileDataArgs;
 
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-    
-    console.log('================');
-    
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+const pageHTML = generatePage(name, github);
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
