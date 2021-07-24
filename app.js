92b09a3073ec07a82983c12b8ccc02148360c5bc
Bri <bhilliker@gmail.com>
2021-07-23 22:33:52 -0500

    add more user information and multiple projects

--- app.js
+++ app.js
@@ -9,12 +9,78 @@ const inquirer = require ('inquirer');
 
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
-inquirer
-.prompt([
-  {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?'
+
+ const promptUser = () => {
+   return inquirer.prompt([
+     {
+       type: 'input',
+       name: 'name',
+       message: 'What is your name?'
+     },
+     {
+      type: 'input',
+      name: 'name',
+      message: 'Enter your HitHub Username'
+    },
+    { 
+      type: 'input',
+      name: 'about',
+      message: 'Provide some informatino about yourself:'
+    }
+   ])
+ }
+ 
+ const promptProject = portfolioData => {
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  } 
+  console.log(`
+   =================
+   Add a New Project
+   =================
+   `);
+   return inquirer.prompt([
+      {
+       type: 'input',
+       name: 'name',
+       message:  'Provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+    ])
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject){
+        return promptProject(portfolioData);
+      } else{
+        return portfolioData;
+      }
+    });
   }
-])
-.then(answers => console.log(answers));
+  
+  promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
